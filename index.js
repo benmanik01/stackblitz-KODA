22f37f8622f297d77bb64c1c54c7297cff562b52
null <null>
2026-04-24 17:17:48 +0700

    level2 for Task1 and Task 2

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+//LEVEL 2- TASK 1
+console.log('Giben Frautama Manik'); //Nama
+console.log(23 + ' Tahun'); //Umur
+console.log('Alamat Jl.Cendrawasi'); //Alamat
+console.log('Musik'); //Hobi
+console.log('Universitas Methodist Indonesia Jurusan Manajemen Informatika'); //Asal Sekolah
+console.log('Ketertarikan saya pada dunia programing berakar dari keselarasan antara bidang ini dengan karakter personal saya (Calm).'); //Alasan Tertarik Programing
+console.log('saya sudah memperlajari dasar dasar-dasar HTML dan CSS untuk membangun struktur dan tampilan website.'); //Hal yang sudah dipelajari berkaitan dengan programming
+console.log('Pernah, kesulitan terbesar saya adalah memahami Konsep Javascript.'); //Pernah Kesulitan ketika belajar programming.
+console.log('saya ingin ikut Bootcamp Koda karena saya ingin belajar terstruktur dan punya RoadMap sesuai dengan dunia pekerjaan.'); //Alasan kenapa ingin ikut Bootcamp Koda.
+console.log('Saya memiliki Tekad yang Kuat dibidang Programing dan memiliki motivasi belajar yang tinggi dan disiplin.'); //Alasan Kenapa saya diterima di Bootcamp Koda
+console.log('');
+//LEVEL 2 - TASK 2
+//Take 1
+let nama ='Giben Frautama Manik';
+let umur =23;
+let alamat ='jalan cendrawasi';
+let hobi ='musik';
+let asalsekolah = 'Universitas Methodist Indonesia';
+let jurusan ='Manajemen Informatika';
+let alasansenangprogramming = 'sebenernya simpel sih, saya ngerasa programming itu gua banget. Saya orangnya sistematis dan suka kerja pakai logika. Pas nyoba ngoding, saya ngerasa nemu dunia yang emang nyambung sama cara kerja otak saya. Jadi, ngerjainnya tuh kerasa seru, bukan kayak beban.';
+//Take 2
+console.log('Nama saya adalah ' + nama + ',asal sekolah dari ' + asalsekolah + ' jurusan ' + jurusan + '.Alasan saya tertarik programing adalah ' + alasansenangprogramming);
+console.log('');
+//Take 3
+console.log('Perbedaan var, let, dengan const adalah soal seberapa bebas dan seberapa permanen variabel tersebut. Singkatnya, var itu fleksibel dan bisa dideklarasikan ulang dan nilanya berubah-ubah , let itu fleksibel untuk nilai yang berubah-ubah dan tidak dapat dideklarasikan ulang diwilayah yang sama, dan const itu harga mati untuk nilai yang tidak boleh diganggu gugat.')
